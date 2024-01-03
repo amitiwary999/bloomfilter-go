29f9d0280c96c4364100ca9593918ba44a3d1ac4
amitiwary999 <tiwarya9@gmail.com>
2024-01-03 23:12:08 +0530

    init version of bloom filter

--- bloom-filter/bloomFilter.go
+++ bloom-filter/bloomFilter.go
@@ -1,1 +1,42 @@
 package bloomfiltergo
+
+import "github.com/spaolacci/murmur3"
+
+func generateHash(str string) []uint32 {
+	data := []byte(str)
+	hashValues := []uint32{}
+	for _, v := range SEED {
+		hashValues = append(hashValues, murmur3.Sum32WithSeed(data, v))
+	}
+	return hashValues
+}
+
+type bloomFilter struct {
+	size   int
+	filter []uint32
+}
+
+func NewBloomFilter(size int) *bloomFilter {
+	return &bloomFilter{
+		size:   size,
+		filter: make([]uint32, size),
+	}
+}
+
+func (b bloomFilter) SetValue(str string) {
+	hashValues := generateHash(str)
+	for _, valu := range hashValues {
+		ind := valu % uint32(b.size)
+		b.filter[ind] = 1
+	}
+}
+
+func (b bloomFilter) GetValuePresent(str string) bool {
+	hashValues := generateHash(str)
+	resp := true
+	for _, valu := range hashValues {
+		ind := valu % uint32(b.size)
+		resp = resp && (b.filter[ind] == 1)
+	}
+	return resp
+}
